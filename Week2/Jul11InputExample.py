bbefdb319b8d4aea008cfe103f0eaa203891a2c4
Sidney Mitchell <sm5069@rit.edu>
2019-07-15 08:54:26 -0400

    Updated notes

--- Week2/Jul11InputExample.py
+++ Week2/Jul11InputExample.py
@@ -4,18 +4,27 @@ clarification.
 '''
 
 def get_age():
-    age = input("How old are you? ")
-    age = int(age)
-    print("In 50 years, you'll be", str(age+50)+".")
-    return age
+    '''
+    This function uses an input to find out the user's age and, using that given value, calculates how old the user will
+    be in 50 years. This function also RETURNS the original value of age to the main function.
+    :return: The age that the user inputs.
+    '''
+    age = input("How old are you? ") #This function asks how old the user is...
+    age = int(age) #Typecasts the age into an integer so it can be added...
+    print("In 50 years, you'll be", str(age+50)+".") #Calculates how old the user will be in 50 years...
+    return age #Returns the original value of age.
+
+    #Notice how the original value "age" is never actually changed by this function.
 
 def get_birth_year(age):
 
-    #Get the current year
+    #Get the current year...
     year = input("What year is it? ")
+
+    #Make that input into an integer...
     year = int(year)
 
-    #Find the birth year
+    #Find the user's birth year.
     birth_year = year-age
 
 def main():
@@ -24,12 +33,13 @@ def main():
     middle_name = input("What is your middle name? ")
     last_name = input("What is your last name? ")
 
+    #Here, your first, middle, and last name will be printed together...
     print(first_name, middle_name, last_name)
 
-    #Store the return value of get_age in a variable called age
+    #Store the return value of get_age in a variable called age...
     age = get_age()
 
-    #Pass the value of age into the get_birth_year function
+    #Pass the value of age into the get_birth_year function...
     get_birth_year(age)
 
 if __name__ == "__main__":
